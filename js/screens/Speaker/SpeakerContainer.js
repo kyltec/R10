19f696b615d074aab8b12498110980f1540c3bf5
Kyle_Tecson <kyltecs@gmail.com>
2019-03-01 17:17:50 -0800

    speaker page shows up again

--- js/screens/Speaker/SpeakerContainer.js
+++ js/screens/Speaker/SpeakerContainer.js
@@ -19,37 +19,13 @@ export default class SessionsContainer extends Component {
     let { navigation } = this.props;
 
     return (
-      <Query
-        query={gql`
-            allSpeakers(filter: { id: $id }) {
-              id
-              bio
-              image
-              name
-              url
-            }
-        `}
-      >
-        {({ loading, error, data }) => {
-          console.log(data);
-          if (loading) {
-            return <ActivityIndicator />;
-          }
-          if (error) {
-            return <Text>Error</Text>;
-          }
-          return (
-            <Speaker
-              navigation={navigation}
-              name={navigation.getParam("name")}
-              bio={navigation.getParam("bio")}
-              url={navigation.getParam("url")}
-              image={navigation.getParam("image")}
-            />
-          );
-        }}
-        //{" "}
-      </Query>
+      <Speaker
+        navigation={navigation}
+        name={navigation.getParam("name")}
+        bio={navigation.getParam("bio")}
+        url={navigation.getParam("url")}
+        image={navigation.getParam("image")}
+      />
     );
   }
 }
